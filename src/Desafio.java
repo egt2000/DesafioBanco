0ea3235bbe3c26af0a67167a794acb9015bd495b
egt2000 <109441235+egt2000@users.noreply.github.com>
2024-05-18 18:43:28 -0300

    incio repo

---
+++ src/Desafio.java
@@ -0,0 +1,12 @@
+public class Desafio {
+    public static void main(String[] args) {
+        String nombre = "Tony Stark";
+        String tipoDeCuenta = "Corriente";
+        double saldo = 1599.99;
+        System.out.println("********************");
+        System.out.println("\nNombre del cliente: " + nombre);
+        System.out.println("El tipo de cuenta es: " + tipoDeCuenta);
+        System.out.println("Su saldo disponible es: " + saldo + "$");
+
+    }
+}
